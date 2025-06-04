69888c82c75357eb5765cc546937103a9a774e60
AZ-1299 <azerukennpi@gmail.com>
2025-06-04 19:50:41 +0900

    update

--- back_end/database/control.py
+++ back_end/database/control.py
@@ -2,17 +2,19 @@ from pathlib import Path
 import sqlite3
 import csv
 import logging
+import pandas as pd
+
 def basicConfig():
     logger = logging.getLogger()
     logger.setLevel(logging.INFO)
 
+
 def CSV2DB(input_FileDir,input_FilePath):
+    in_col = ["コード"]
     input_FileAbspath = input_FileDir/input_FilePath
     print(f"INFO: {input_FileAbspath}")
-    with open(input_FileAbspath,"r",encoding = "UTF-8_sig") as f:
-        reader = csv.reader(f)
-        for row in reader:
-            print(row)
+    df_main = pd.read_csv(input_FileAbspath,encoding="utf-8")
+    print(df_main)
     
     
 
@@ -20,5 +22,7 @@ if __name__ == "__main__":
     self_path = Path(__file__)
     parents_dir = self_path.resolve().parents[1]
     print(f"inpout is   {parents_dir}")
+    TSE_data_path = self_path/".."/"static"/"TSE_data.csv"
+
     input_FileDir = parents_dir /"input_data"
     CSV2DB(input_FileDir,"user_portfolio_special.csv")
