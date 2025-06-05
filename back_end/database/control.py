fcb6e42b30c944015ee3b763f32de708ddebb9e6
AZ-1299 <azerukennpi@gmail.com>
2025-06-05 14:38:47 +0900

    update

--- back_end/database/control.py
+++ back_end/database/control.py
@@ -12,23 +12,26 @@ def basicConfig():
 def CSV2DB(input_FileDir,input_FilePath,TSE_data_path):
     logging.info("CSV2DB読み込み完了")
     input_FileAbspath = input_FileDir/input_FilePath
+    Output_df = []
 
     # TSEデータの下処理
     in_col_TSE = ["コード","17業種区分"]
+    in_col_TSE_search = "コード"
     TSE_df = pd.read_csv(TSE_data_path,encoding="cp932")
     logging.info("TSEデータ読み込み完了")
-    new_TSE_df = TSE_df[in_col_TSE]
-    # print(TSE_df)
+    new_TSE_df = TSE_df.loc[:,in_col_TSE]
+    search_TSE_df =TSE_df.loc[:,in_col_TSE_search]
 
     # ユーザポートフォリオcsv
     in_col = ["コード"]
     df_main = pd.read_csv(input_FileAbspath,encoding="utf-8")
-    logging.info("road user csv")
-    new_df_main =df_main.iloc[:,1:7]
+    logging.info("ユーザポートフォリオ読み込み完了")
     
-    # print(new_TSE_df)   
-    # for row in new_df_main:
-    #     print(row)
+    #検索
+    for i in df_main:
+        for j in search_TSE_df:
+            if i==j:
+                
 
 if __name__ == "__main__":
     self_path = Path(__file__)
