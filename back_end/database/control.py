46ea019728e857fc2a912e3093ce8b4523700f73
AZ <azerukennpi@gmail.com>
2025-06-12 18:55:26 +0900

    update

--- back_end/database/control.py
+++ back_end/database/control.py
@@ -16,36 +16,29 @@ def CSV2DB(input_FileDir,input_FilePath,TSE_data_path):
     input_FileAbspath = input_FileDir/input_FilePath
 
     # TSEデータの下処理
-    in_col_TSE = ["コード","17業種区分"]
-    in_col_TSE_search = "コード"
+    in_col_TSE = ["コード","33業種区分"]
     TSE_df = pd.read_csv(TSE_data_path, encoding="cp932", dtype={"コード": str})
+    # print(TSE_df)
     logging.info("TSEデータ読み込み完了")
     new_TSE_df = TSE_df.loc[:,in_col_TSE]
-    # search_TSE_df =TSE_df.loc[:,in_col_TSE_search]
 
     # ユーザポートフォリオcsv
     in_col = ["コード"]
     df_main = pd.read_csv(input_FileAbspath, encoding="utf-8", dtype={"コード": str})
     df_main_code = df_main.loc[:,in_col]
     # print(df_main)
-    # print(df_main_code)
     logging.info("ユーザポートフォリオ読み込み完了")
 
-    merged_df = pd.merge(df_main_code, new_TSE_df, on="コード", how="right")
+    merged_df = pd.merge(df_main_code, new_TSE_df, on="コード", how="left")
 
     # print(merged_df)
 
-    # print(search_TSE_df)
-    # for i in df_main_code:
-        # print(i)
+    merged_df = pd.merge(df_main, merged_df, on="コード", how="left")
+    merged_df = merged_df.drop(columns=["買付日","前日比", "前日比（％）"])
+    print(merged_df)
 
-    # #検索
-    # for i in df_main_code:
-    #     logging.info(f"検索内容は {i}")
-    #     for j in in_col_TSE:
-    #         if i==j:
-    #             logging.info(f"検索にヒット。ヒット= {j}")
-    # logging.info("検索終了")
+
+
 
 if __name__ == "__main__":
     basicConfig()
