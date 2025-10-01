95bea71a1a23a4509ae7f32cc2afc3e7ae9ecb7d
AZ <azerukennpi@gmail.com>
2025-10-01 09:59:53 +0900

    update

--- front_end/gui/Controller.java
+++ front_end/gui/Controller.java
@@ -2,11 +2,11 @@ package front_end.gui;
 
 import java.awt.Component;
 import java.io.*;
-import java.nio.file.*;
-import javax.swing.JFileChooser;
 // import javax.swing.JPanel;
 // import javax.swing.JScrollPane;
 // import javax.swing.JTable;
+import java.nio.file.*;
+import javax.swing.JFileChooser;
 
 import java.util.logging.Logger;
 import java.util.logging.Level;
@@ -15,7 +15,6 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 // import java.sql.Statement;
-
 
 public class Controller {
 
@@ -34,10 +33,10 @@ public class Controller {
             File selectedFile = filechooser.getSelectedFile();
             try {
                 Path input_path = Paths.get(
-                    selfPath,
-                 "..",
-                    "back_end",
-                    "input_data");
+                        selfPath,
+                        "..",
+                        "back_end",
+                        "input_data");
                 logger.info("INFO: input_path is:" + input_path);
                 File inputDir = input_path.toFile();
                 if (!inputDir.exists()) {
@@ -66,7 +65,7 @@ public class Controller {
         // File go2py_path = currentDir_next.toFile();
         String pythonScriptPath = scriptPath.toAbsolutePath().toString();
         ProcessBuilder go2py = new ProcessBuilder("python", "-u", pythonScriptPath);
-       
+
         go2py.redirectOutput(ProcessBuilder.Redirect.INHERIT);
         go2py.redirectError(ProcessBuilder.Redirect.INHERIT);
         System.out.println("INFO:実行フォイル" + pythonScriptPath);
@@ -89,17 +88,21 @@ public class Controller {
             desp_portfolio();
         }
     }
-    
-    public void desp_portfolio(){
+
+    public void desp_portfolio() {
         System.out.println("desp_portfolio 実行");
-        Path currentPath = Paths.get("").toAbsolutePath();
-        Path dbPath = currentPath.resolve(Paths.get(
-        "back_end",
-            "database",
-            "user_data",
-            "user_database.db")).normalize();
+        Path selfPath = Paths.get("").toAbsolutePath();
+        String string_seflPath = selfPath.toString();
+        System.out.println("string_seflPath is : " + string_seflPath);
+        Path dbPath = selfPath.resolve(Paths.get(
+                "back_end",
+                "database",
+                "user_data",
+                "user_database.db")).normalize();
         String url = "jdbc:sqlite:" + dbPath.toString();
 
+        String string_dbpath = dbPath.toString();
+        System.out.println("dbPath is : " + string_dbpath);
         Connection conn = null;
         try {
             Class.forName("org.sqlite.JDBC");
