04efba6795540c40d47bcb62190614d220f94d7d
AZ <azerukennpi@gmail.com>
2025-10-09 23:33:38 +0900

    update

--- run.bat
+++ run.bat
@@ -1,2 +1,2 @@
-javac -b front_end/bin/ front_end/gui/*.java
-java -classpath lib/sqlite-jdbc.jar front_end/gui/Com
+javac -d .\front_end\bin\ .\front_end\gui\*java
+java -classpath .\lib\sqlite-jdbc.jar .\front_end\gui\Common_obj.java
