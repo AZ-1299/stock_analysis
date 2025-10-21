0bdb966a508f52f4657521e8d054e13d24c2b6f4
AZ <azerukennpi@gmail.com>
2025-10-21 09:17:26 +0900

    update

---
+++ front_end/control/DataUpdate.java
@@ -0,0 +1,32 @@
+package front_end.control;
+
+import javax.swing.table.AbstractTableModel;
+import java.util.List;
+import front_end.control.Connect_DB.PortfolioRow;
+
+public class DataUpdate extends AbstractTableModel {
+    
+    private List<PortfolioRow> data;
+    private final String[] columnNames = {"コード", "銘柄名", "数量", "取得単価", "業種", "口座"};
+
+    public DataUpdate(List<PortfolioRow> data) {
+        this.data = data != null ? data : new java.util.ArrayList<>();
+    }
+    //列数把握
+    @Override
+    public int getColumnCount() { return columnNames.length; }
+    //行数把握
+    @Override
+    public int getRowCount() { return data.size(); }
+    //ヘッダー名把握
+    @Override
+    public String getColumnName(int col) { return columnNames[col]; }
+    //有効な行番号把握
+    @Override
+    public Object getValueAt(int row, int col) {
+        if (row >= 0 && row < data.size()) {
+            return data.get(row).toArray()[col];
+        }
+        return null;
+    }
+}
