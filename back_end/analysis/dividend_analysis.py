60dd74b9139f0f1070024511cc4fc1ab2a17de32
AZ <azerukennpi@gmail.com>
2025-11-30 16:04:31 +0900

    update

--- back_end/analysis/dividend_analysis.py
+++ back_end/analysis/dividend_analysis.py
@@ -1,26 +1,25 @@
 #年間配当予想
 #セクター別配当比率
 #業種
+
 import numpy as np
 from pathlib import Path
 import sqlite3
-
-def connect_db():
-    open_path = parents_dir/"database"/"user_data"/"user_database.db" 
-    print(open_path)
-    conn = sqlite3.connect(open_path)
-    cur = conn.cursor()
-    Industry_ratio ="SELECT NAME "
-
 def desp_db(parents_dir):
-    open_path = parents_dir/"database"/"user_data"/"user_database.db" 
-    desp_dbconn  = "SELECT * FROM user_database"
-    conn = sqlite3.connect(open_path)
-    cur = conn.cursor()
-    cur.execute(desp_dbconn)
+    open_path = parents_dir.joinpath("database", "user_data", "user_database.db")    
+    desp_dbconn  = 'SELECT * FROM user_database'
+    try:
+        conn = sqlite3.connect(open_path)
+        cur = conn.cursor()
+        cur.execute(desp_dbconn)
+        rows = cur.fetchall()
+        print(f"--- 取得データ ({len(rows)}件) ---")
+        for row in rows:
+            print(row)
+
+    except:
+        print("エラー")
     
-    # for row in cur.execute(desp_dbconn):
-    #         print(row)
 
 if __name__ =='__main__':
     print("dividend_analysis.pyに接続")
