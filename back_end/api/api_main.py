c691e52c0687948580f1456f43c00936878c2118
AZ-1299 <azerukennpi@gmail.com>
2026-02-06 09:48:47 +0900

    update

--- back_end/api/api_main.py
+++ back_end/api/api_main.py
@@ -1,20 +1,36 @@
 import pandas as pd
 import sqlite3
 from pathlib import Path
+import yfinance as yf
+import datetime
 
-def db_connect(con_path):
+
+def db_getCode(con_path):
     print("db_connect 関数開始")
+
+    codes = []
+
     get_code = "SELECT code FROM user_database"
     con = sqlite3.connect(con_path)
     cur = con.cursor()
     cur.execute(get_code)
-    rows = cur.fetchall()
-    select_sql = 'SELECT * FROM user_database'
-    for row in cur.execute(select_sql):
-        print(row)
+    codes = [row[0] for row in cur.fetchall()]
+    return codes
 
-def now_price():
+def now_price(codes,ut):
     print("now_price関数開始")
+    ticker_codes = [f"{code}.T" for code in codes]
+    print(ticker_codes)
+
+    now_price = []
+    for i in ticker_codes:
+        now_price.append(yf.download(tickers=i,period='1d',interval="1d"))
+    
+    print(now_price)
+    return now_price
+
+    
+
 
 if __name__ == "__main__":
     print("input_data.py 開始")
@@ -26,4 +42,11 @@ if __name__ == "__main__":
         "user_database.db"
     )
     print(f"DB Path: {con_path}")
-    db_connect(con_path)
+    codes = db_getCode(con_path)
+    print("get codes")
+    print(codes)
+
+    dt = datetime.datetime.today()
+    ut = dt.date()
+    print(f"Now time is {ut}")
+    new_now_price = now_price(codes,ut)
